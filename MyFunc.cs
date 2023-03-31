656dbc80b6cc593d32e83060c0ccdf7145637924
Assad Faizi <assad.faizi@maersk.com>
2023-03-31 09:45:22 +0100

    Initial Setup

---
+++ MyFunc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleFuncApp;
+
+public static class MyFunc
+{
+    [Function("MyFunc")]
+    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("MyFunc");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString(value: $"Hello {Environment.GetEnvironmentVariable("MyName")} From Azure");
+
+        return response;
+        
+    }
+}
